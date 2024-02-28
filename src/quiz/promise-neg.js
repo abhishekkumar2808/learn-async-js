f5727856c760ad0ee0e2296ee17afc329f9653e0
abhishekkumar <kumar.abhishe@northeastern.edu>
2024-02-28 15:19:12 -0500

    02272024

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,38 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function negPerRow(arr) {
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(Array.isArray(arr)) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr.length; i++) {
+
+                        sum += arr[i];
+
+                }
+                console.log('resolving ... ');
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from sum');
+    });
+}
+
+Promise.any(array2D.map((i) => sum1DArray(i) ))
+.then((sumArray) =>{
+
+    let sum = 0;
+    for( let i = 0 ; i < sumArray.length; i++)
+        sum += sumArray[i];
+
+        console.log("sum: ", sum);
+})
+
+//put a catch at the end of a then
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,37 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+
+function sum1DArray(arr) {
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(Array.isArray(arr)) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr.length; i++) {
+
+                        sum += arr[i];
+
+                }
+                console.log('resolving ... ');
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from sum');
+    });
+}
+
+Promise.all(array2D.map((i) => sum1DArray(i) ))
+.then((sumArray) =>{
+
+    let sum = 0;
+    for( let i = 0 ; i < sumArray.length; i++)
+        sum += sumArray[i];
+
+        console.log("sum: ", sum);
+})
